1467bd018e16b30ac3b4c4581f1ebb03b30ba112
Nick Partridge <nkpart@gmail.com>
2010-02-06 12:26:51 +1000

    Property fully abstracted on the 'source'. Could now be an entity, request, map, function, whatever.

--- src/main/scala/sage/package.scala
+++ src/main/scala/sage/package.scala
@@ -3,13 +3,13 @@ import Scalaz._
 import com.google.appengine.api.datastore._
 
 package object sage {  
-  implicit def newtypeProp[T <: AnyRef, U <: NewType[T]](f: T => U)(implicit m: ClassManifest[U], tm: ClassManifest[T]): P1[U] = {
+  implicit def newtypeProp[T <: AnyRef, U <: NewType[T]](f: T => U)(implicit m: ClassManifest[U], tm: ClassManifest[T]): P1[U, Entity] = {
     stringProp(m.erasure.getSimpleName.toLowerCase).typedProp[U, T](f)
   }
   
   implicit def stringProp(s: String) = new {
     
-    def typedProp[T <: NewType[U], U <: AnyRef](f: U => T)(implicit m: ClassManifest[U]) = new P1[T] {
+    def typedProp[T <: NewType[U], U <: AnyRef](f: U => T)(implicit m: ClassManifest[U]) = new P1[T, Entity] {
       val delegate = prop[U]
       
       def get(e: Entity) = delegate.get(e) map (u => f(u))
@@ -18,7 +18,7 @@ package object sage {
       def put(t: T, e: Entity) = delegate.put(t.value, e)
     }
     
-    def prop[T <: AnyRef](implicit m: ClassManifest[T]) = new P1[T] {
+    def prop[T <: AnyRef](implicit m: ClassManifest[T]) = new P1[T, Entity] {
       def get(e: Entity) = {
         val o = Option(e.getProperty(s)) match {
           case v@Some(a: T) if (m.erasure.isAssignableFrom(a.getClass)) => v.asInstanceOf[Option[T]]
